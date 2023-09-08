d28dfe1234da0f282d96a5fe4c81354e707346f6
krishnagudladana <krishnagc108@gmail.com>
2023-09-08 23:06:07 +0530

    completed

---
+++ src/Api.jsx
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "9a75bc9ab3msh120c453a8caeaf4p1433a8jsnb760af2686bc",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "84a4439b4841075bf7a9b44b07c4ef9c";
